9f2aa7cee184d903b4ab5b2057d4292b207657d5
Michael Arcidiacono <mixarcidiacono@gmail.com>
2019-01-17 20:09:17 -0800

    sensor interface works

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
+#include <SPI.h>
+#include <SD.h>
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
+#include <Adafruit_BMP280.h>
 
+#define BMP_SCK  (14)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+
+Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+const int chipSelect = BUILTIN_SDCARD;
+
+void error() {
+  while (true) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
+  }
+}
 
 void setup() {
   
@@ -17,18 +37,49 @@ void setup() {
   Wire.setSDA(38);
   Wire.setSCL(37);
 
-  while(!bno.begin()){
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(100);
+  if (!bno.begin()) {
+    Serial.println("ALACK! I have no BNOs!");
+    error();
+  }
+
+  if (!SD.begin(chipSelect)) {
+    Serial.println("ALACK! I have no SD cards!");
+    error();
+  }
+
+  if (!bmp.begin()) {
+    Serial.println("ALACK! I have no bmps!");
+    error();
   }
 
 }
 
 void loop() {
+
+  File dataFile = SD.open("LAUNCH_DATA.csv", FILE_WRITE);
+  if (!dataFile) {
+    Serial.println("ALACK! I have no files!!");
+    error();
+  }
+
+  sensors_event_t event; 
+  bno.getEvent(&event);
+
+  dataFile.print(event.orientation.x, 16);
+  dataFile.print(", ");
+  dataFile.print(event.orientation.y, 16);
+  dataFile.print(", ");
+  dataFile.print(event.orientation.z, 16);
+  dataFile.print(", ");
+  dataFile.print(bmp.readTemperature(), 16);
+  dataFile.print(", ");
+  dataFile.print(bmp.readPressure(), 16);
+  dataFile.print(", ");
+  dataFile.print(bmp.readAltitude(1013.25), 16);
+  dataFile.println("");
+  
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(50);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  delay(50);
 }
